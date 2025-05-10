efb4de02407e5366b7d7a762edfed0da764ecc94
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-05-10 14:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("timer");
+function setTime(){
+	const date = new Date();
+	para.textContent = date.toLocaleString();
+}
+setInterval(setTime,1000);
